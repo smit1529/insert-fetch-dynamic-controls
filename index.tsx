c033636e98d56783c0607c766441b5981cd129fe
smit1529 <smit.pshah92@gmail.com>
2019-03-27 14:36:23 +0530

    1 file modified, 1 deleted

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
+// import Hello from './Hello';
 import './style.css';
 import DynamicControl from './DynamicControl'
 
